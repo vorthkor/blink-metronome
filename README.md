da28923acc9fb5c7bb0e8f1c3d4af49a6019d46b
Victor Passos <vicfilipe@gmail.com>
2021-10-23 19:21:38 -0300

    Update READEME.md and add metronome script

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # blink-metronome
+
+This metronome helps you when the sound distracts you training music.
+Soon: You can turn on and off both LED and buzzer, you choose what to use.
+
+It's set to 4/4 60bpm. Working on new features.
+
+## Usage
+
+The `GPIO.setmode(GPIO.BCM)` declared means that you need to use the port name, not its physical number.
+To know both just execute `pinout` on your pi's terminal and use the `GPIO(x)` number.
+If you want to use the physical, change this line to `GPIO.setmode(GPIO.BOARD)`.
+
+I'm using RGB LED but you can use 1 simple LED or two to simulate the higher tempo.
+
+## References
+
+- [How to use RGB LED][rp]
+- [Examples on pinout usage][pu]
+- [Buzzer usage][bu]
+- [Interrupts on raspi][ir]
+- [Beats calculator][bc]
+- [Python time's sleep][pt]
+- [Using timer][ut]
+
+* * *
+
+> "Thanks for passing by."
+
+  [rp]: https://www.instructables.com/Raspberry-Pi-Tutorial-How-to-Use-a-RGB-LED/
+  [pu]: https://raspi.tv/2013/rpi-gpio-basics-4-setting-up-rpi-gpio-numbering-systems-and-inputs
+  [bu]: https://projects.raspberrypi.org/en/projects/rpi-connect-buzzer
+  [ir]: https://raspi.tv/2013/how-to-use-interrupts-with-python-on-the-raspberry-pi-and-rpi-gpio
+  [bc]: https://toolstud.io/music/bpm.php?bpm=100&bpm_unit=4%2F4
+  [pt]: https://docs.python.org/3/library/time.html#time.sleep
+  [ut]: https://stackoverflow.com/questions/377454/how-do-i-get-my-python-program-to-sleep-for-50-milliseconds
--- metronome.py
+++ metronome.py
@@ -1,6 +1,21 @@
 # libraries
 import RPi.GPIO as GPIO
+import os
 from time import sleep
+
+# INITIAL MESSAGE
+os.system('clear')
+print('\n Welcome to the blinking metronome project!')
+print('''\
+                                _                          
+                  __      _____| | ___ ___  _ __ ___   ___ 
+                  \ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ |
+                   \ V  V /  __/ | (_| (_) | | | | | |  __/
+                    \_/\_/ \___|_|\___\___/|_| |_| |_|\___|
+                                                            
+
+                ''')
+print('\n Press ctrl+C to exit...\n')
 
 # disable warnings (optional)
 GPIO.setwarnings(False)
@@ -8,79 +23,63 @@ GPIO.setwarnings(False)
 # Select GPIO Mode
 GPIO.setmode(GPIO.BCM)
 
-# set red,green and blue pins
+# set red,green, blue and buzzer pins
 redPin = 12
-greenPin = 19
-bluePin = 13
+bluePin = 19
+greenPin = 13
+buzzerPin = 23
 
 # set pins as outputs
 GPIO.setup(redPin, GPIO.OUT)
 GPIO.setup(greenPin, GPIO.OUT)
 GPIO.setup(bluePin, GPIO.OUT)
+GPIO.setup(buzzerPin, GPIO.OUT)
 
 
 def turnOff():
-    GPIO.output(redPin, GPIO.HIGH)
-    GPIO.output(greenPin, GPIO.HIGH)
-    GPIO.output(bluePin, GPIO.HIGH)
-
-
-def white():
     GPIO.output(redPin, GPIO.LOW)
     GPIO.output(greenPin, GPIO.LOW)
     GPIO.output(bluePin, GPIO.LOW)
+    GPIO.output(buzzerPin, GPIO.LOW)
 
 
 def red():
-    GPIO.output(redPin, GPIO.LOW)
-    GPIO.output(greenPin, GPIO.HIGH)
-    GPIO.output(bluePin, GPIO.HIGH)
+    GPIO.output(redPin, GPIO.HIGH)
+    GPIO.output(greenPin, GPIO.LOW)
+    GPIO.output(bluePin, GPIO.LOW)
 
 
 def green():
-    GPIO.output(redPin, GPIO.HIGH)
-    GPIO.output(greenPin, GPIO.LOW)
-    GPIO.output(bluePin, GPIO.HIGH)
+    GPIO.output(redPin, GPIO.LOW)
+    GPIO.output(greenPin, GPIO.HIGH)
+    GPIO.output(bluePin, GPIO.LOW)
 
 
 def blue():
-    GPIO.output(redPin, GPIO.HIGH)
-    GPIO.output(greenPin, GPIO.HIGH)
-    GPIO.output(bluePin, GPIO.LOW)
-
-
-def yellow():
     GPIO.output(redPin, GPIO.LOW)
     GPIO.output(greenPin, GPIO.LOW)
     GPIO.output(bluePin, GPIO.HIGH)
 
 
-def purple():
-    GPIO.output(redPin, GPIO.LOW)
+def yellow():
+    GPIO.output(redPin, GPIO.HIGH)
     GPIO.output(greenPin, GPIO.HIGH)
     GPIO.output(bluePin, GPIO.LOW)
 
 
-def lightBlue():
-    GPIO.output(redPin, GPIO.HIGH)
-    GPIO.output(greenPin, GPIO.LOW)
-    GPIO.output(bluePin, GPIO.LOW)
+def main():
+    while True:
+        red()
+        sleep(1)
+        green()
+        sleep(1)
+        blue()
+        sleep(1)
+        green()
+        sleep(1)
 
 
-while True:
+try:
+    main()
+except KeyboardInterrupt:
     turnOff()
-    sleep(1)  # 1second
-    white()
-    sleep(1)
-    red()
-    sleep(1)
-    green()
-    sleep(1)
-    blue()
-    sleep(1)
-    yellow()
-    sleep(1)
-    purple()
-    sleep(1)
-    lightBlue()
-    sleep(1)
